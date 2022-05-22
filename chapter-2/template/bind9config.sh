4fabcef8bb3d5bc6bf3e84f2d00bc84e0914b925
David Okeyode <asegunlolu@hotmail.com>
2022-05-22 23:21:23 +0100

    modified bind9 shell script

--- chapter-2/template/bind9config.sh
+++ chapter-2/template/bind9config.sh
@@ -11,11 +11,5 @@ apt install dnsutils -y
 # add the DNS zones
 curl https://raw.githubusercontent.com/davidokeyode/AZ-700/main/chapter-2/template/bindconfig -o /etc/bind/named.conf.local
 
-# add the zone file
-curl https://raw.githubusercontent.com/davidokeyode/AZ-700/main/chapter-2/template/zonefile -o /etc/bind/db.internal.charistech.xyz
-
-# add the reverse zone file
-curl https://raw.githubusercontent.com/davidokeyode/AZ-700/main/chapter-2/template/reversezonefile -o /etc/bind/db.10.10.reverse
-
 # restart the DNS service
 systemctl restart bind9.service
